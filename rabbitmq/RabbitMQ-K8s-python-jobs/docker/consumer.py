afaf8cb80811da629c7147ae94d14231f0279c18
marcoBrighterAI <marco@brighter.ai>
2021-07-26 12:30:46 +0200

    feat: add logging to the consumer script

--- rabbitmq/RabbitMQ-K8s-python-jobs/docker/consumer.py
+++ rabbitmq/RabbitMQ-K8s-python-jobs/docker/consumer.py
@@ -9,17 +9,24 @@ import sys
 import os
 import pika
 import signal
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+
 
 def queue_callback(channel, method, properties, body):
-  if len(method.exchange):
-    print("from exchange '{}': {}".format(method.exchange,body.decode('UTF-8')))
-  else:
-    print("from queue {}: {}".format(method.routing_key,body.decode('UTF-8')))
+    if len(method.exchange):
+        logging.info("from exchange '{}': {}".format(method.exchange, body.decode('UTF-8')))
+    else:
+        logging.info("from queue {}: {}".format(method.routing_key, body.decode('UTF-8')))
 
-def signal_handler(signal,frame):
-  print("\nCTRL-C handler, cleaning up rabbitmq connection and quitting")
-  connection.close()
-  sys.exit(0)
+
+def signal_handler(signal, frame):
+    logging.info("\nCTRL-C handler, cleaning up rabbitmq connection and quitting")
+    connection.close()
+    sys.exit(0)
+
 
 example_usage = '''====EXAMPLE USAGE=====
 
@@ -33,20 +40,19 @@ Specify exchange and queue name
 ap = argparse.ArgumentParser(description="RabbitMQ producer",
                              epilog=example_usage,
                              formatter_class=argparse.RawDescriptionHelpFormatter)
-ap.add_argument('--user',default="guest",help="username e.g. 'guest'")
-ap.add_argument('--password',default="guest",help="password e.g. 'pass'")
-ap.add_argument('--host',default="localhost",help="rabbitMQ host, defaults to localhost")
-ap.add_argument('--port',type=int,default=5672,help="rabbitMQ port, defaults to 5672")
-ap.add_argument('--exchange',default="",help="name of exchange to use, empty means default")
-ap.add_argument('--queue',default="testqueue",help="name of default queue, defaults to 'testqueue'")
-ap.add_argument('--routing-key',default="testqueue",help="routing key, defaults to 'testqueue'")
-ap.add_argument('--body',default="my test!",help="body of message, defaults to 'mytest!'")
+ap.add_argument('--user', default="guest", help="username e.g. 'guest'")
+ap.add_argument('--password', default="guest", help="password e.g. 'pass'")
+ap.add_argument('--host', default="localhost", help="rabbitMQ host, defaults to localhost")
+ap.add_argument('--port', type=int, default=5672, help="rabbitMQ port, defaults to 5672")
+ap.add_argument('--exchange', default="", help="name of exchange to use, empty means default")
+ap.add_argument('--queue', default="testqueue", help="name of default queue, defaults to 'testqueue'")
+ap.add_argument('--routing-key', default="testqueue", help="routing key, defaults to 'testqueue'")
+ap.add_argument('--body', default="my test!", help="body of message, defaults to 'mytest!'")
 args = ap.parse_args()
 
-
 # connect to RabbitMQ
-credentials = pika.PlainCredentials(args.user, args.password )
-connection = pika.BlockingConnection(pika.ConnectionParameters(args.host, args.port, '/', credentials ))
+credentials = pika.PlainCredentials(args.user, args.password)
+connection = pika.BlockingConnection(pika.ConnectionParameters(args.host, args.port, '/', credentials))
 channel = connection.channel()
 
 channel.basic_consume(queue=args.queue, on_message_callback=queue_callback, auto_ack=True)
@@ -54,7 +60,6 @@ channel.basic_consume(queue=args.queue, on_message_callback=queue_callback, auto
 # capture CTRL-C
 signal.signal(signal.SIGINT, signal_handler)
 
-print("Waiting for messages, CTRL-C to quit...")
-print("")
+logging.info("Waiting for messages, CTRL-C to quit...")
+logging.info("")
 channel.start_consuming()
-
